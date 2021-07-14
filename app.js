8fcf0dfc6a2c86183f0c6d33227a1028ebd024ec
tluwaniMS <tluwaniMS2020@gmail.com>
2021-07-14 18:42:33 +0200

    adding-app-configs-middleware

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const {
+  unknownRequests,
+  errorResponse
+} = require("./middleware/error-handler");
 const app = express();
 
 dotenv.config();
+
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+
+app.use(unknownRequests);
+app.use(errorResponse);
 
 module.exports = {
-	app
+  app
 };

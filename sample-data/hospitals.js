7dd195083a2ae6b130604cd38e1722968a29cafd
tluwaniMS <tluwaniMS2020@gmail.com>
2021-07-30 19:30:06 +0200

    adding-hospitals-sample-data

--- sample-data/hospitals.js
+++ sample-data/hospitals.js
@@ -1,3 +1,57 @@
-const hospitals = [];
+const hospitals = [
+	{
+		hospitalName: 'Sekhukhune Private Hospital',
+		hospitalKey: 'SKKN',
+		municipality: ''
+	},
+	{
+		hospitalName: 'Shapo Private Hospital',
+		hospitalKey: 'SHPO',
+		municipality: ''
+	},
+	{
+		hospitalName: 'Polokwane Hospital',
+		hospitalKey: 'PLKH',
+		municipality: ''
+	},
+	{
+		hospitalName: 'Botlokwa Hospital',
+		hospitalKey: 'BTLK',
+		municipality: ''
+	},
+	{
+		hospitalName: 'Marobjyane Private Hospital',
+		hospitalKey: 'MRBJ',
+		municipality: ''
+	},
+	{
+		hospitalName: 'Ntsundeni Private Hospital',
+		hospitalKey: 'NTNP',
+		municipality: ''
+	},
+	{ hospitalName: 'Moshe Hospital', hospitalKey: 'MSHH', municipality: '' },
+	{
+		hospitalName: 'Marabastad Hospital',
+		hospitalKey: 'MRSB',
+		municipality: ''
+	},
+	{ hospitalName: 'Baloyi Hospital', hospitalKey: 'BLYH', municipality: '' },
+	{
+		hospitalName: 'Vhatwanamba Private Health Care',
+		hospitalKey: 'VTPH',
+		municipality: ''
+	},
+	{ hospitalName: 'Bulombo Hospital', hospitalKey: 'BLMH', municipality: '' },
+	{
+		hospitalName: 'Mashashane Hospital',
+		hospitalKey: 'MSSH',
+		municipality: ''
+	},
+	{
+		hospitalName: 'dendron Health Centre',
+		hospitalKey: 'DNDH',
+		municipality: ''
+	}
+];
 
 module.exports = { hospitals };

b9db9580fbf33524075f2d9a5615f7387a8a156b
tluwaniMS <tluwaniMS2020@gmail.com>
2021-07-30 19:15:13 +0200

    adding-doctors-data

--- sample-data/doctors.js
+++ sample-data/doctors.js
@@ -1,3 +1,229 @@
-const doctors = [];
+const doctors = [
+	{
+		firstName: 'Dean',
+		lastName: 'Hill',
+		gender: 'Male',
+		password: 'dean1234',
+		email: 'dean@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Lufuno',
+		lastName: 'Sekgothe',
+		gender: 'Female',
+		password: 'lufu1234',
+		email: 'lufuno@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Refilwe',
+		lastName: 'Phago',
+		gender: 'Female',
+		password: 'refi1234',
+		email: 'refilwe@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Seba',
+		lastName: 'Sibika',
+		gender: 'Female',
+		password: 'seba1234',
+		email: 'seba@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Katlego',
+		lastName: 'Mamani',
+		gender: 'Female',
+		password: 'katl1234',
+		email: 'katlego@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Idah',
+		lastName: 'Leseme',
+		gender: 'Female',
+		password: 'idah1234',
+		email: 'idah@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Jessica',
+		lastName: 'Shadung',
+		gender: 'Female',
+		password: 'jess1234',
+		email: 'jessica@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Tshego',
+		lastName: 'Seoka',
+		gender: 'Male',
+		password: 'tshe1234',
+		email: 'tshego@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Jethro',
+		lastName: 'Mokgohloa',
+		gender: 'Male',
+		password: 'jeth1234',
+		email: 'jethro@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Kgaogelo',
+		lastName: 'Munyangane',
+		gender: 'Female',
+		password: 'kgao1234',
+		email: 'kgaogelo@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Mmadi',
+		lastName: 'Ndlovu',
+		gender: 'Male',
+		password: 'mmad1234',
+		email: 'mmadi@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Abigail',
+		lastName: 'Sekhosana',
+		gender: 'Female',
+		password: 'abig1234',
+		email: 'abigail@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Mmakosha',
+		lastName: 'Deagrela',
+		gender: 'Female',
+		password: 'mmak1234',
+		email: 'mmakosha@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Dorcus',
+		lastName: 'Maruma',
+		gender: 'Female',
+		password: 'dorc1234',
+		email: 'dorcus@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Denzel',
+		lastName: 'Mophosho',
+		gender: 'Male',
+		password: 'denz1234',
+		email: 'denzel@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Lerato',
+		lastName: 'Sehume',
+		gender: 'Female',
+		password: 'lera1234',
+		email: 'lerato@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Londokuhle',
+		lastName: 'Ndala',
+		gender: 'Male',
+		password: 'lond1234',
+		email: 'londokuhle@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Thovhedzo',
+		lastName: 'Maosa',
+		gender: 'Male',
+		password: 'thov1234',
+		email: 'thovhedzo@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Reneilwe',
+		lastName: 'Dikana',
+		gender: 'Female',
+		password: 'rene1234',
+		email: 'reneilwe@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Philicia',
+		lastName: 'Sothobe',
+		gender: 'Female',
+		password: 'phil1234',
+		email: 'philicia@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Eddy',
+		lastName: 'Manaka',
+		gender: 'Male',
+		password: 'eddy1234',
+		email: 'eddy@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Mathibela',
+		lastName: 'Serumula',
+		gender: 'Male',
+		password: 'math1234',
+		email: 'mathibela@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Mpho',
+		lastName: 'Leshabane',
+		gender: 'Male',
+		password: 'mpho1234',
+		email: 'mpho@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Lehlohonolo',
+		lastName: 'Sebata',
+		gender: 'Male',
+		password: 'lehl1234',
+		email: 'lehlohonolo@mock.com',
+		hospital: '',
+		specialty: ''
+	},
+	{
+		firstName: 'Mpumi',
+		lastName: 'Khoza',
+		gender: 'Male',
+		password: 'mpum1234',
+		email: 'mpumi@mock.com',
+		hospital: '',
+		specialty: ''
+	}
+];
 
 module.exports = { doctors };

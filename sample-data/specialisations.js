80232f4b3dc811b511541a7631a339b3f553ac5d
tluwaniMS <tluwaniMS2020@gmail.com>
2021-07-30 20:38:39 +0200

    adding-specialisations-sample-data

--- sample-data/specialisations.js
+++ sample-data/specialisations.js
@@ -1,3 +1,11 @@
-const specialisations = [];
+const specialisations = [
+	{ specialisationName: 'Pediatrician', specialisationKey: 'PDTN' },
+	{ specialisationName: 'Dematologist', specialisationKey: 'DMTG' },
+	{ specialisationName: 'Cardiologist', specialisationKey: 'CDGT' },
+	{ specialisationName: 'Urologist', specialisationKey: 'URLG' },
+	{ specialisationName: 'Neurologist', specialisationKey: 'NRLG' },
+	{ specialisationName: 'Psychiatrist', specialisationKey: 'PSCT' },
+	{ specialisationName: 'Radiologist', specialisationKey: 'RDLG' }
+];
 
 module.exports = { specialisations };

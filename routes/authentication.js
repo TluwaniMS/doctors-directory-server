208c7a0c80b957aee5b8d872329d483123a77549
tluwaniMS <tluwani2020@gmail.com>
2021-09-12 23:57:04 +0200

    adding-authentication-requests-responses

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,17 +4,23 @@ const { errorHandler } = require('../middleware/error-handler');
 
 router.post(
 	'/sign-in',
-	errorHandler(async (req, res) => {})
+	errorHandler(async (req, res) => {
+		res.status(200).send({});
+	})
 );
 
 router.post(
 	'/register-user',
-	errorHandler(async (req, res) => {})
+	errorHandler(async (req, res) => {
+		res.status(200).send({});
+	})
 );
 
 router.put(
 	'/reset-user-password',
-	errorHandler(async (req, res) => {})
+	errorHandler(async (req, res) => {
+		res.status(200).send({});
+	})
 );
 
 module.exports = router;

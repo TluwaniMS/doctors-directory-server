a1b63d28d31dd3222fed0687b1950b830dc4a6c7
tluwaniMS <tluwani2020@gmail.com>
2021-10-02 16:29:37 +0200

    adding-first-aggregation-function

---
+++ database-services/doctors-stats-service.js
@@ -0,0 +1,16 @@
+const { Doctor } = require('../models/doctor');
+const { sequelize } = require('../database-config');
+
+async function getDoctorsStatsGroupedByGender() {
+	const doctorsGroupedByGenderCount = await Doctor.findAll({
+		attributes: [
+			'gender',
+			[sequelize.fn('COUNT', sequelize.col('gender')), 'n_gender']
+		],
+		group: ['gender']
+	});
+
+	return doctorsGroupedByGenderCount;
+}
+
+module.exports = { getDoctorsStatsGroupedByGender };
--- routes/directory-statistics.js
+++ routes/directory-statistics.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {
+	getDoctorsStatsGroupedByGender
+} = require('../database-services/doctors-stats-service');
 const { errorHandler } = require('../middleware/error-handler');
+
+router.get(
+	'/doctors-grouped-by-gender',
+	errorHandler(async (req, res) => {
+		const doctorsGroupedByGenderCount =
+			await getDoctorsStatsGroupedByGender();
+
+		res.status(200).send({ data: doctorsGroupedByGenderCount });
+	})
+);
 
 module.exports = router;

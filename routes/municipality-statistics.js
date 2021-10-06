57a427da772b875716f24127c6e1a3fa76a44a44
tluwaniMS <tluwani2020@gmail.com>
2021-10-06 22:30:13 +0200

    adding-municipality-stats-end-point

--- routes/municipality-statistics.js
+++ routes/municipality-statistics.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+router.get(
+	'/get-stats-count-by-municipality-grouped-by-hospital',
+	errorHandler(async (req, res) => {
+		res.status(200).send({ data: '' });
+	})
+);
+
+router.get(
+	'/get-stats-count-by-municipality-grouped-by-specialty',
+	errorHandler(async (req, res) => {
+		res.status(200).send({ data: '' });
+	})
+);
+
+router.get(
+	'/get-stats-count-by-municipality-grouped-by-gender',
+	errorHandler(async (req, res) => {
+		res.status(200).send({ data: '' });
+	})
+);
+
+router.get(
+	'/get-stats-count-by-municipality-grouped-by-gender-and-specialty',
+	errorHandler(async (req, res) => {
+		res.status(200).send({ data: '' });
+	})
+);
 module.exports = router;

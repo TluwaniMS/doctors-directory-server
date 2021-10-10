d077585541984dba74083e5f8bd8a35ca24ceb92
tluwaniMS <tluwani2020@gmail.com>
2021-10-10 04:05:39 +0200

    updating-specialty-model-properties

--- model-properties/specialty-model-properties.js
+++ model-properties/specialty-model-properties.js
@@ -1,8 +1,32 @@
 module.properties = {
 	SpecialtyProperties: {
 		Pediatrician: {
-			SpecialtyName: '',
-			SpecialtyKey: ''
+			SpecialtyName: 'Pediatrician',
+			SpecialtyKey: 'PDTN'
+		},
+		Dematologist: {
+			SpecialtyName: 'Dematologist',
+			SpecialtyKey: 'DMTG'
+		},
+		Cardiologist: {
+			SpecialtyName: 'Cardiologist',
+			SpecialtyKey: 'CDGT'
+		},
+		Urologist: {
+			SpecialtyName: 'Urologist',
+			SpecialtyKey: 'URLG'
+		},
+		Neurologist: {
+			SpecialtyName: 'Neurologist',
+			SpecialtyKey: 'NRLG'
+		},
+		Psychiatrist: {
+			SpecialtyName: 'Psychiatrist',
+			SpecialtyKey: 'PSCT'
+		},
+		Radiologist: {
+			SpecialtyName: 'Radiologist',
+			SpecialtyKey: 'RDLG'
 		}
 	}
 };

54992b861bc32d5113a18d68d828ed36484a67af
tluwaniMS <tluwani2020@gmail.com>
2021-10-17 10:17:43 +0200

    final-pieces-to-municipality-statistics-route-end-point-and-service

--- routes/municipality-statistics.js
+++ routes/municipality-statistics.js
@@ -17,89 +17,55 @@ const {
 } = require('../auxiliary-services/municipality-stats-service');
 
 router.get(
-	'/get-gender-count-of-municipality-doctors/:municipalityKey',
+	'/get-municipality-statistics/:municipalityKey',
 	errorHandler(async (req, res) => {
 		const { municipalityKey } = req.params;
+
 		const municipalitiesGenderCount =
 			await getMunicipalityWithHospitalsAndNestedDoctorGendersByKey(
 				municipalityKey
 			);
-
-		const municipalitiesWithGenderCount = calculateMunicipalityGenderCount(
-			municipalitiesGenderCount
-		);
-
-		res.status(200).send({ data: municipalitiesWithGenderCount });
-	})
-);
-
-router.get(
-	'/get-specialty-count-of-municipalities-doctors/:municipalityKey',
-	errorHandler(async (req, res) => {
-		const { municipalityKey } = req.params;
 		const municipalityDoctorsSpecialtyCount =
 			await getMunicipalityWithHospitalsAndNestedDoctorSpecialtiesByKey(
 				municipalityKey
 			);
-
-		const municipalityWithTotalSpecialtyCount =
-			calculateMunicipalitySpecialityCount(
-				municipalityDoctorsSpecialtyCount
-			);
-
-		res.status(200).send({ data: municipalityWithTotalSpecialtyCount });
-	})
-);
-
-router.get(
-	'/get-specialty-count-of-municipalities-grouped-by-gender/:municipalityKey',
-	errorHandler(async (req, res) => {
-		const { municipalityKey } = req.params;
 		const municipalitiesSpecialtyCountGroupedByGender =
 			await getMunicipalityWithHospitalsAndNestedDoctorSpecialtiesAndGenderByKey(
 				municipalityKey
 			);
-
-		const municipalitiesWithSpecialtyCountsGroupedByGender =
-			calculateMunicipalitySpecialitiesGroupedByGenderCount(
-				municipalitiesSpecialtyCountGroupedByGender
-			);
-
-		res.status(200).send({
-			data: municipalitiesWithSpecialtyCountsGroupedByGender
-		});
-	})
-);
-
-router.get(
-	'/get-total-doctors-count-of-municipalities/:municipalityKey',
-	errorHandler(async (req, res) => {
-		const { municipalityKey } = req.params;
 		const municipalitiesWithHospitalsAndNestedDoctors =
 			await getMunicipalityWithHospitalsAndNestedDoctorsByKey(
 				municipalityKey
 			);
-
-		const municipalitiesWithTotalDoctorCount =
-			calculateMunicipalityDoctorsCount(
-				municipalitiesWithHospitalsAndNestedDoctors
-			);
-
-		res.status(200).send({ data: municipalitiesWithTotalDoctorCount });
-	})
-);
-
-router.get(
-	'/get-total-hospital-count-of-municipalities/:municipalityKey',
-	errorHandler(async (req, res) => {
-		const { municipalityKey } = req.params;
 		const municipalitiesWithHospitals =
 			await getMunicipalityNestedWithHospitalsByKey(municipalityKey);
 
 		const municipalitiesWithHospitalCount =
 			calculateMunicipalityHospitalCount(municipalitiesWithHospitals);
-
-		res.status(200).send({ data: municipalitiesWithHospitalCount });
+		const municipalitiesWithGenderCount = calculateMunicipalityGenderCount(
+			municipalitiesGenderCount
+		);
+		const municipalitiesWithTotalDoctorCount =
+			calculateMunicipalityDoctorsCount(
+				municipalitiesWithHospitalsAndNestedDoctors
+			);
+		const municipalitiesWithSpecialtyCountsGroupedByGender =
+			calculateMunicipalitySpecialitiesGroupedByGenderCount(
+				municipalitiesSpecialtyCountGroupedByGender
+			);
+		const municipalityWithTotalSpecialtyCount =
+			calculateMunicipalitySpecialityCount(
+				municipalityDoctorsSpecialtyCount
+			);
+		res.status(200).send({
+			totalHospitalsInMunicipality: municipalitiesWithHospitalCount,
+			totalGenderCountInMunicipality: municipalitiesWithGenderCount,
+			totalDoctorCountInMunicipality: municipalitiesWithTotalDoctorCount,
+			totalSpecialtyCountGroupedByGenderInMunicipality:
+				municipalitiesWithSpecialtyCountsGroupedByGender,
+			totalSpecialtyCountInMunicipality:
+				municipalityWithTotalSpecialtyCount
+		});
 	})
 );
 
